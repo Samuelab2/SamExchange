6d31e907b331ca26905d2c384018f56edc9e4613
Samuel Mata <samuelab2@gmail.com>
2020-02-14 13:08:08 -0400

    adding a convertor in coin details and fix some layouts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <main>
-    <sx-header />
+    <sx-header :links="links" />
     <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 
 <script>
-import SxHeader from "@/components/SxHeader";
+import SxHeader from '@/components/SxHeader'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     SxHeader
+  },
+
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'XRP',
+          to: { name: 'coin-detail', params: { id: 'ripple' } }
+        }
+      ]
+    }
   }
-};
+}
 </script>
 
 <style>
--- src/components/SxHeader.vue
+++ src/components/SxHeader.vue
@@ -13,7 +13,16 @@
         <div
           class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              v-for="l in links"
+              :key="l.title"
+              :to="l.to"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {{ l.title }}</router-link
+            >
+          </div>
         </div>
       </nav>
     </nav>
@@ -21,10 +30,16 @@
 </template>
 
 <script>
-import SxIcon from "@/components/SxIcon";
+import SxIcon from '@/components/SxIcon'
 
 export default {
-  name: "SxHeader",
-  components: { SxIcon }
-};
+  name: 'SxHeader',
+  components: { SxIcon },
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
 </script>
